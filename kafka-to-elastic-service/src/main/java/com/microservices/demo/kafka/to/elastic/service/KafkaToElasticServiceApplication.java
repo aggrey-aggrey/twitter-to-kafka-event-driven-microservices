37e5f9fe11fa0aafb449e669da2a742c5e1e6f03
aggrey-aggrey <iluv2code2@gmail.com>
2023-02-18 13:12:43 -0600

    added elatic-index-client module, elasticSearchRepositories for indexing, integrated elastic modules with microservice and containerized the service

--- kafka-to-elastic-service/src/main/java/com/microservices/demo/kafka/to/elastic/service/KafkaToElasticServiceApplication.java
+++ kafka-to-elastic-service/src/main/java/com/microservices/demo/kafka/to/elastic/service/KafkaToElasticServiceApplication.java
@@ -1,12 +1,14 @@
 package com.microservices.demo.kafka.to.elastic.service;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.microservices.demo")
-public class KafkaToElasticSearchApplication {
+public class KafkaToElasticServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(KafkaToElasticSearchApplication.class, args);
+        SpringApplication.run(KafkaToElasticServiceApplication.class, args);
     }
 }
+
